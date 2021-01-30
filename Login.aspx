330197694ebed0d8f9688c0ce88146c725471dbb
BurairAbbas <syedburair528@gmail.com>
2021-01-30 09:17:17 +0500

    Uploaded

---
+++ Login.aspx
@@ -0,0 +1,203 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 121px;
+        }
+        .auto-style2 {
+            width: 89px;
+            height: 30px;
+        }
+        .auto-style3 {
+            width: 90px;
+            height: 30px;
+        }
+        .auto-style4 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="background-color: burlywood; height: 384px; width: 709px;">
+            <div style="float: left">
+                <table style="height: 383px; width: 339px">
+                    <tr>
+                        <td>ID No:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="IDtb" runat="server" placeholder =""></asp:TextBox>
+                        </td>
+                        <td>
+                            <asp:Button ID="Findbt" runat="server" Text="Find" OnClick="Findbt_Click" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <!----Lable---->
+                        <td colspan="2" style="padding:0px;margin:0px">
+                            <asp:Label ID="lblMsg" runat="server" Text=""  ></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>Emp Name:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="Emptb" runat="server" placeholder="Enter Your Name!!"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Father Name:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="FNametb" runat="server" placeholder="Enter Your Father Name!!" ></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Contact No:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="Contact_tb" runat="server" placeholder ="Enter Contact Number!!"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>CNIC No:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="CNICtb" runat="server" placeholder ="Enter CNIC Number!!"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Address:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="Addresstb" runat="server" TextMode="MultiLine" Height="50px" Width="155px" placeholder ="Enter your Address!!"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Gender:
+                        </td>
+                        <td>
+                            <asp:RadioButton ID="RadioButton1" runat="server" Text="Male" />&nbsp;&nbsp;
+                        <asp:RadioButton ID="RadioButton2" runat="server" Text="Female" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Basic Salary:
+                        </td>
+                        <td>
+                            <asp:TextBox ID="BSalarytb" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+
+
+                </table>
+            </div>
+
+            <!---second div---->
+            <div style="float: left">
+                <table style="height: 89px; width: 339px">
+                    <tr>
+                        <td>                            
+                            <asp:Image ID="Image1" runat="server" BorderStyle="Dashed" Width="150px" Height="110px" style="text-align:center" AlternateText="Upload for Image Here..." />
+                        </td>
+                        <td class="auto-style1">
+                            <asp:FileUpload ID="FileUpload1" runat="server" Width="181px" />
+                            <asp:Button ID="Uploadbt" runat="server" Text="Upload" OnClick="Uploadbt_Click" />
+                            <asp:Label ID="Label1" runat="server" Text="Label" Visible="False"></asp:Label>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>Designation:
+                        </td>
+                        <td class="auto-style1">
+                            <!-- You can add listItem here and as well as click on dropdown their is small arrow on right click on it Add Existed item and Add your item from desige -->
+                            <asp:DropDownList ID="DesigList" runat="server">
+                                
+                                <asp:ListItem></asp:ListItem>
+                                <asp:ListItem Value="2">Front-End</asp:ListItem>                                
+                                <asp:ListItem Value="3">Back-End</asp:ListItem>
+                                <asp:ListItem Value="4">Manager</asp:ListItem>
+                                <asp:ListItem Value="5">Team-Lead</asp:ListItem>
+                                
+                                <asp:ListItem Value="1">Full-Stack</asp:ListItem>
+                                
+                            </asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Department:
+                        </td>
+                        <td class="auto-style1">
+                            <asp:DropDownList ID="DepartList" runat="server">
+                                <asp:ListItem></asp:ListItem>
+                                <asp:ListItem Value="2">Client Service</asp:ListItem>
+                                <asp:ListItem Value="3">Software House</asp:ListItem>
+                                <asp:ListItem Value="4">Gound Depart</asp:ListItem>
+                                <asp:ListItem Value="5">MIS</asp:ListItem>
+                                <asp:ListItem Value="1">HR</asp:ListItem>
+                            </asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>DOB:
+                        </td>
+                        <td class="auto-style1">
+                            <asp:TextBox ID="DOBtb" runat="server" placeholder="dd/mm/yyyy"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>DOJ:
+                        </td>
+                        <td class="auto-style1">
+                            <asp:TextBox ID="DOJtb" runat="server" placeholder="dd/mm/yyyy"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>DOL:
+                        </td>
+                        <td class="auto-style1">
+                            <asp:TextBox ID="DOLtb" runat="server" placeholder="Enter Days!!"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Type of Leave:
+                        </td>
+                        <td class="auto-style1">
+                            <asp:DropDownList ID="DropDownList3" runat="server">
+                                <asp:ListItem Value="1">Sick leave</asp:ListItem>
+                                <asp:ListItem Value="2">Vacation Leave</asp:ListItem>
+                            </asp:DropDownList>
+                        </td>
+                    </tr>
+                    
+                </table>
+            </div>
+            <div style ="float:right; width: 370px; height: 78px; background-color:burlywood">
+                <table>
+                    <tr>
+                        <td style="padding-right: 30px" class="auto-style2">
+                            <asp:Button ID="Inserttb" runat="server" Text="Insert" Width="67px" Height="39px" Font-Bold="true" OnClick="Inserttb_Click" />
+                            </td>
+                        <td style="padding:30px" class="auto-style3">
+                            <asp:Button ID="Updatetb" runat="server" Text="Update" Width="67px" Height="39px" Font-Bold="true" OnClick="Updatetb_Click"/>
+                        </td>
+                        <td class="auto-style4"  style ="padding:30px" >
+                            <asp:Button ID="Deletebt" runat="server" Text="Delete" Width="67px" Height="39px" Font-Bold="true" OnClick="Deletebt_Click"/>
+                        </td>                      
+                    </tr>                   
+                </table>
+            </div>
+        </div>
+        
+
+        <asp:Label ID="ImageName" runat="server" Visible="False"></asp:Label>
+        
+
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // this login form take my three days approx. without image upload 
+    }
+
+    protected void Findbt_Click(object sender, EventArgs e)
+    {
+
+        //    DB_Class dbc = new DB_Class();
+        //    DataTable DT;
+        //    DT = dbc.ReadDataTable("select * from Emp_LoginForm where ID = " + IDtb.Text);
+
+        //    // if i put Cleartxtbx method in start it set the value of IDtb.Text to ""(Empty) which give error in above ReadDataTable 
+        //    Cleartxtbx();
+        //    if (DT.Rows.Count > 0)
+        //    {
+
+        //        Emptb.Text = DT.Rows[0]["Emp_Name"].ToString().Trim();
+        //        FNametb.Text = DT.Rows[0]["FName"].ToString().Trim();
+        //        Contact_tb.Text = DT.Rows[0]["Contact_No"].ToString().Trim();
+        //        CNICtb.Text = DT.Rows[0]["CNIC_No"].ToString().Trim();
+        //        Addresstb.Text = DT.Rows[0]["Address"].ToString().Trim();
+        //        BSalarytb.Text = DT.Rows[0]["Basic_Salary"].ToString().Trim();
+        //        DOBtb.Text = DT.Rows[0]["DOB"].ToString().Trim();
+        //        DOJtb.Text = DT.Rows[0]["DOJ"].ToString().Trim();
+        //        DOLtb.Text = DT.Rows[0]["DOL"].ToString().Trim();
+
+        //        //if (DesigList.SelectedValue !=  DT.Rows[0]["Desig_ID"].ToString())
+        //        DesigList.SelectedValue = DT.Rows[0]["Desig_ID"].ToString();
+        //        //if (DepartList.SelectedValue != DT.Rows[0]["Depart_ID"].ToString())
+        //        DepartList.SelectedValue = DT.Rows[0]["Depart_ID"].ToString();
+        //}
+    }
+
+    protected void Inserttb_Click(object sender, EventArgs e)
+    {
+        if (IDtb.Text == "")
+        {
+            if (BSalarytb.Text != "")
+            {
+                // Change Desig_ID from selectedValue to Selected index so if user not, gives any value there still 0 value goes in Query. 
+                string SqlInsertQuery = "insert into Emp_LoginForm(ID,Emp_Name,FName,Contact_No,CNIC_No,Address,Basic_Salary,DOB,DOJ,DOL,Desig_ID,Depart_ID,Image)" +
+                    " values ((select Max(ID) + 1 from Emp_LoginForm),'" + Emptb.Text + "','" + FNametb.Text + "','" + Contact_tb.Text + "','" +
+                    CNICtb.Text + "','" + Addresstb.Text + "'," + BSalarytb.Text + ",'" + DOBtb.Text + "','" + DOJtb.Text + "','" + DOLtb.Text + "'," + DesigList.SelectedIndex + "," + DepartList.SelectedIndex + ",'" + ImageName.Text + "')";
+
+                DB_Class dbc = new DB_Class();
+
+                if (dbc.MyExecuteNonQuery(SqlInsertQuery))
+                {
+                    lblMsg.Text = "Successfully Execute";
+                }
+                else
+                {
+                    lblMsg.Text = "Error";
+                }
+                Cleartxtbx();
+            }
+            else
+            {
+                lblMsg.Text = "Salary Needed";
+            }
+        }
+        else
+        {
+            lblMsg.Text = "ID Not Required";
+        }
+    }
+    protected void Updatetb_Click(object sender, EventArgs e)
+    {
+
+        if (IDtb.Text == "")
+        {
+            lblMsg.Text = "ID Required";
+        }
+        else
+        {
+            //to get the already exist value in database so it will not remove by blank space in new update. 
+            DB_Class dbc = new DB_Class();
+            DataTable DT;
+            DT = dbc.ReadDataTable("select * from Emp_LoginForm where ID = " + IDtb.Text);
+            if (DT.Rows.Count > 0)
+            {
+                // if Emp textbox is empty put the value from database EmpName in it!!
+                if (Emptb.Text == "")
+                    Emptb.Text = DT.Rows[0]["Emp_Name"].ToString().Trim();
+                if (FNametb.Text == "")
+                    FNametb.Text = DT.Rows[0]["FName"].ToString().Trim();
+                if (Contact_tb.Text == "")
+                    Contact_tb.Text = DT.Rows[0]["Contact_No"].ToString().Trim();
+                if (CNICtb.Text == "")
+                    CNICtb.Text = DT.Rows[0]["CNIC_No"].ToString().Trim();
+                if (Addresstb.Text == "")
+                    Addresstb.Text = DT.Rows[0]["Address"].ToString().Trim();
+                if (BSalarytb.Text == "")
+                {
+                    BSalarytb.Text = DT.Rows[0]["Basic_Salary"].ToString().Trim();
+                    // if salary is till empty then we show msg to get salary by user
+                    if (BSalarytb.Text == "")
+                        lblMsg.Text = "Salary is Required";
+                }
+                if (DOBtb.Text == "")
+                    DOBtb.Text = DT.Rows[0]["DOB"].ToString().Trim();
+                if (DOJtb.Text == "")
+                    DOJtb.Text = DT.Rows[0]["DOJ"].ToString().Trim();
+                if (DOLtb.Text == "")
+                    DOLtb.Text = DT.Rows[0]["DOL"].ToString().Trim();
+                // if desig value is not value to the desig value of current id than change the desig value
+                if (DesigList.SelectedValue != DT.Rows[0]["Desig_ID"].ToString())
+                    DesigList.SelectedValue = DT.Rows[0]["Desig_ID"].ToString();
+                if (DepartList.SelectedValue != DT.Rows[0]["Depart_ID"].ToString())
+                    DepartList.SelectedValue = DT.Rows[0]["Depart_ID"].ToString();
+                if (ImageName.Text == "")
+                    ImageName.Text = DT.Rows[0]["Image"].ToString();
+                // these value are not shown in form becoz they is ClearTb ethod called in the last of Form.
+
+            }
+
+            //Little bit complicated that why i break it in strings and concatenate in string variable
+
+            //string SqlUpdateQuery = "update Emp_LoginForm set Emp_Name = '" + Emptb.Text + "',FName = '" + FNametb.Text + "',Contact_No = '" + Contact_tb.Text + "'" +
+            //    ", CNIC_No = '" + CNICtb.Text + "', Address =  '" + Addresstb.Text + "', Basic_Salary = " + BSalarytb.Text +
+            //    ", Desig_ID = '" + DesigList.SelectedValue + "', Depart_ID = '" + DepartList.SelectedValue + "' where ID = '" + IDtb.Text + "'";
+
+            // Emp_Name and FName
+            string Names = "Emp_Name = '" + Emptb.Text + "',FName = '" + FNametb.Text + "'";
+            //Contact and Cnic Number
+            string ConAndCnic = "Contact_No = '" + Contact_tb.Text + "', CNIC_No = '" + CNICtb.Text + "'";
+            //Address and Salary
+            string AddAndSal = "Address =  '" + Addresstb.Text + "', Basic_Salary = " + BSalarytb.Text;
+            //Designation and Department list
+            string Lists = "Desig_ID = '" + DesigList.SelectedValue + "', Depart_ID = '" + DepartList.SelectedValue + "'";
+            //Date of Birth/Job/Leave
+            string Dates = "DOB = '" + DOBtb.Text + "', DOJ = '" + DOJtb.Text + "', DOL = '" + DOLtb.Text + "'";
+            //for Image
+            string Image = "Image = '" + ImageName.Text + "'";
+
+            // declare variable above to make the code clean
+            string SqlUpdateQuery = "update Emp_LoginForm set " + Names + "," + ConAndCnic + "," + AddAndSal + "," + Lists + "," + Dates + "," + Image + " where ID = '" + IDtb.Text + "'";
+
+            if (dbc.MyExecuteNonQuery(SqlUpdateQuery))
+            {
+                lblMsg.Text = "Successfully Execute";
+            }
+            else
+            {
+                lblMsg.Text = "Error";
+            }
+            // to clear the textboxes innthe form
+            Cleartxtbx();
+        }
+
+    }
+
+    //To Clear Every Textbox 
+    protected void Cleartxtbx()
+    {
+        IDtb.Text = "";
+        Emptb.Text = "";
+        FNametb.Text = "";
+        Contact_tb.Text = "";
+        CNICtb.Text = "";
+        Addresstb.Text = "";
+        BSalarytb.Text = "";
+        DOBtb.Text = "";
+        DOJtb.Text = "";
+        DOLtb.Text = "";
+        Image1.ImageUrl = "";
+    }
+    protected void Deletebt_Click(object sender, EventArgs e)
+    {
+        if (IDtb.Text == "")
+        {
+            lblMsg.Text = "ID Required";
+        }
+        else
+        {
+            string SqlDelQuery = "Delete from Emp_LoginForm where ID = '" + IDtb.Text + "'";
+            DB_Class dbc = new DB_Class();
+            if (dbc.MyExecuteNonQuery(SqlDelQuery))
+            {
+                lblMsg.Text = "Successfully Execute";
+            }
+            else
+            {
+                lblMsg.Text = "Error";
+            }
+            Cleartxtbx();
+        }
+    }
+
+    protected void Uploadbt_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            string ext = Path.GetExtension(FileUpload1.FileName);
+            if (ext == ".jpg" || ext == ".png")
+            {
+                //get the filename from the uploaded file
+                string path = Path.GetFileName(FileUpload1.FileName);
+
+                //replace blank spaces from the filename
+                path = path.Replace(" ", "");
+
+                //save selected image in the folder 'Images' right-top with path name 
+                FileUpload1.SaveAs(Server.MapPath("~/Images/") + path);
+
+                //Full Path of Image
+                string imageName = "~/Images/" + path;
+                Image1.ImageUrl = imageName;
+
+                //Label name to save the 'Path' of image. put the image name in LabelText becoz 
+                //it become easier to call 'Path' in other button located at left button after form 
+                ImageName.Text = imageName;
+
+                //Display For User
+                Label1.Visible = true;
+                Label1.Text = "Uploaded";
+            }
+            else
+            {
+                Label1.Visible = true;
+                Label1.Text = "Upload Only png and jpg";
+            }
+        }
+        else
+        {
+            Label1.Visible = true;
+            Label1.Text = "Please Upload File";
+        }
+    }
+}
